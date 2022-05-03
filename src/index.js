b0f2495a50771b9d6ec527ae8d443432f5c4ede0
Fatimata Diallo <fatimatadiallo8@hotmail.com>
2022-05-03 08:45:00 +0200

    correction exos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("application react");
+import {
+  bonjour,
+  bonjour2,
+  displayNotes,
+  displayNotesClassique,
+  displayStudentName,
+  student,
+  calculateAverageClassique,
+  calculateAverage,
+  displayStudent,
+} from './exos/revision.js'
+
+console.log(bonjour('John'))
+console.log(bonjour2('john'))
+console.log(displayStudentName(student))
+console.log(displayNotesClassique(student.notes))
+console.log(calculateAverage(student.notes))
+console.log(calculateAverageClassique(student.notes))
+console.log(displayStudent(student))
